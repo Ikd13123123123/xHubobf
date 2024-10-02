e1655915cd0d7a0a0d04e4e1457cf104106af920
xBackpack <jamescheeseman21@gmail.com>
2024-10-02 18:25:04 +0100

    fixes

--- places/12552538292.lua
+++ places/12552538292.lua
@@ -577,7 +577,7 @@ library:GiveSignal(rooms.ChildAdded:Connect(function(room)
         end
     end
 
-    setupDoorESP(room:WaitForChild("Entrances"):WaitForChild("NormalDoor"))
+    setupDoorESP(room:WaitForChild("Entrances"):WaitForChild("NormalDoor"):WaitForChild("Door"))
 end))
 
 library:GiveSignal(runService.RenderStepped:Connect(function()
